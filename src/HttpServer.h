be4031ac9900060bfab00084aba1cb1508305acc
ian287913 <45512766+ian287913@users.noreply.github.com>
2021-05-04 15:32:54 +0800

    Clang format HttpServer.h

--- src/HttpServer.h
+++ src/HttpServer.h
@@ -6,13 +6,13 @@ Use of this source code is governed by a BSD-style license that can be
 found in the LICENSE file. See the AUTHORS file for names of contributors.
  */
 #include <iostream>
+
 #include "ThreadPool.h"
 
 #ifndef SHORTLINK_SRC_HTTPSERVER_H_
 #define SHORTLINK_SRC_HTTPSERVER_H_
 
-namespace shortlink
-{
+namespace shortlink {
 
 // Handles the connection from all the clients.
 // HttpServer will establish the ListenerThreadPool during the construction.
@@ -20,34 +20,35 @@ namespace shortlink
 //    std::unique_ptr<HttpServer> httpServer = make_unique<HttpServer>();
 //    // start the server
 //    httpServer->Start();
-public class HttpServer
-{
-public:
-    // Constructor
-	HttpServer();
-    // Delete copy constructors
-    HttpServer(const HttpServer&) = delete;
-    HttpServer& operator=(const HttpServer&) = delete;
-    // Destructor
-	~HttpServer();
-    // Start the http server
-    void Start();
-    // Close the http server
-    void Shutdown();
-    // Get the server status
-    int GetServerStatus();
-    // Get the current number of clients
-    int GetClientCount();
-    // Determines the size of the listenerThreadPool
-    const int maxClientCount = 10;
-private:
-    // Thread pool initialization. This will be call by the constructor.
-    void InitializeListenerThreadPool();
-    ThreadPool listenerThreadPool;
-    int serverStatus = 0;
-    int clientCount = 0;
+public
+class HttpServer {
+ public:
+  // Constructor
+  HttpServer();
+  // Delete copy constructors
+  HttpServer(const HttpServer &) = delete;
+  HttpServer &operator=(const HttpServer &) = delete;
+  // Destructor
+  ~HttpServer();
+  // Start the http server
+  void Start();
+  // Close the http server
+  void Shutdown();
+  // Get the server status
+  int GetServerStatus();
+  // Get the current number of clients
+  int GetClientCount();
+  // Determines the size of the listenerThreadPool
+  const int maxClientCount = 10;
+
+ private:
+  // Thread pool initialization. This will be call by the constructor.
+  void InitializeListenerThreadPool();
+  ThreadPool listenerThreadPool;
+  int serverStatus = 0;
+  int clientCount = 0;
 }
 
-}   // namespace shortlink
+}  // namespace shortlink
 
-#endif // !SHORTLINK_SRC_HTTPSERVER_H_
+#endif  // !SHORTLINK_SRC_HTTPSERVER_H_
