6bb0991e0b1fa5db7a0c60f6a0b0a493ee84de29
will889889 <will889889@gmail.com>
2021-05-04 12:06:50 +0800

    Move file to src folder : SearchHandler.h
    Clang formatted

--- src/SearchHandler.h
+++ src/SearchHandler.h
@@ -1,6 +1,5 @@
 /*
-Copyright (c) 2021 Chiu Yen-Chen,
-Swen Sun-Yen.
+Copyright (c) 2021 Chiu Yen-Chen, Swen Sun-Yen, Wen Yong-Wei, Yuan Wei-Chen.
 All rights reserved.
 Use of this source code is governed by a BSD-style license that can be
 found in the LICENSE file. See the AUTHORS file for names of contributors.
@@ -9,23 +8,21 @@ found in the LICENSE file. See the AUTHORS file for names of contributors.
 #define SHORTLINK_SRC_SEARCHHANDLER_H_
 #include <string>
 
-namespace shortlink
-{
+namespace shortlink {
 
 // This class represents a operation of searching for URL by SURL
 // Example:
 //    SearchHandler handler = SearchHandler();
 //    string rawURL = handler.SearchSURL();
-class SearchHandler
-{
-public:
-	SearchHandler();
-	~SearchHandler();
+class SearchHandler {
+ public:
+  SearchHandler();
+  ~SearchHandler();
 
-	// Search by the SURL in the database, return the original URL.
-	std::string SearchURL();
+  // Search by the SURL in the database, return the original URL.
+  std::string SearchURL();
 };
 
-}
+}  // namespace shortlink
 
-#endif // SHORTLINK_SRC_SEARCHHANDLER_H_
+#endif  // SHORTLINK_SRC_SEARCHHANDLER_H_
