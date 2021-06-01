5293bfd9fad275d44b3ba7a62bc32647e1513d08
will889889 <will889889@gmail.com>
2021-06-01 17:45:40 +0800

    Add cmake and main

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+/*
+Copyright (c) 2021 Chiu Yen-Chen, Swen Sun-Yen, Wen Yong-Wei, Yuan Wei-Chen.
+All rights reserved.
+Use of this source code is governed by a BSD-style license that can be
+found in the LICENSE file. See the AUTHORS file for names of contributors.
+ */
+#include<iostream>
+
+int main()
+{
+    std::cout << "Hello, Here is Short Link Program." << std::endl;
+    return 0;
+}
