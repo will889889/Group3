e3bee90bb8c2f8341894ff23a8447fb158c9faff
firewings2515 <capricorn9908@gmail.com>
2021-06-27 22:36:45 +0800

    Create Database.cpp file

---
+++ src/Database.cpp
@@ -0,0 +1,104 @@
+/*
+Copyright (c) 2021 Chiu Yen-Chen, Swen Sun-Yen, Wen Yong-Wei, Yuan Wei-Chen.
+All rights reserved.
+Use of this source code is governed by a BSD-style license that can be
+found in the LICENSE file. See the AUTHORS file for names of contributors.
+ */
+
+#include "Database.h"
+
+namespace shortlink {
+std::string Database::CreateSURL(const std::string &raw_url) {
+  // Create the short url
+  std::string short_url = SURLCreator::CreateSURL(raw_url);
+
+  // Check if short url exists
+  std::string read_url = ReadSURL(short_url);
+  if (read_url != "") {
+    // if short url exists
+    if (read_url == raw_url) {
+      // the short url and its corresponding raw_url in lut is the same as the
+      // input raw_url. Update the surl expire time.
+      Date now = boost::gregorian::day_clock::local_day();
+      UpdateSURL(std::move(lut.at(short_url)), now);
+      return read_url;
+    } else {
+      // the short url in lut is the same as the input short url,
+      // but the corresponding raw_url is not the same.
+      // Add the offset to Encode the url
+      int offset = 1;
+      while (read_url != raw_url) {
+        short_url = SURLCreator::CreateSURL(raw_url, offset);
+        read_url = ReadSURL(short_url);
+      }
+      std::unique_ptr<SURL> surl(new SURL(raw_url, short_url));
+      InsertSURL(short_url, std::move(surl));
+      return short_url;
+    }
+  } else {
+    // if short url does not in the lut, insert it to the lut
+    std::unique_ptr<SURL> surl(new SURL(raw_url, short_url));
+    InsertSURL(short_url, std::move(surl));
+    return short_url;
+  }
+}
+std::string Database::ReadSURL(const std::string &surl) {
+  // if the surl is not in the lut, return "", else return the raw url.
+  write_lock.lock();
+  bool found_flag = (lut.find(surl) != lut.end());
+  write_lock.unlock();
+  if (!found_flag) {
+    return "";
+  }
+
+  return lut.at(surl)->GetUrl();
+}
+bool Database::InsertSURL(const std::string &short_url,
+                          std::unique_ptr<SURL> surl) {
+  // lock the insert lock, make sure there is only one thread can change lut.
+  write_lock.lock();
+  lut[short_url] = std::move(surl);
+  write_lock.unlock();
+  return true;
+}
+bool Database::UpdateSURL(std::unique_ptr<SURL> surl, const Date &expire) {
+  write_lock.lock();
+  surl.get()->SetExpireTime(expire);
+  lut.at(surl.get()->GetSURL()) = std::move(surl);
+  write_lock.unlock();
+  return true;
+}
+
+bool Database::CheckSURLExpiration() {
+  Date now = boost::gregorian::day_clock::local_day();
+
+  boost::gregorian::date_duration dur = now - last_expiration;
+  if (dur.days() > 1) {
+    return DeleteSURL();
+  }
+
+  return false;
+}
+
+bool Database::DeleteSURL() {
+  Date now = boost::gregorian::day_clock::local_day();
+  bool delete_flag = false;
+
+  std::unordered_map<std::string, std::unique_ptr<SURL>>::iterator it;
+  it = lut.begin();
+  while (it != lut.end()) {
+    const boost::gregorian::date_duration kExpireTime =
+        now - it->second.get()->GetExpireTime();
+    const int kExpireTimeInDays = kExpireTime.days();
+    if (kExpireTimeInDays >= 30) {
+      write_lock.lock();
+      it = lut.erase(it);
+      delete_flag = true;
+      write_lock.unlock();
+    }
+  }
+
+  return delete_flag;
+}
+
+} // namespace shortlink
--- src/Database.h
+++ src/Database.h
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2021 Chiu Yen-Chen, Swen Sun-Yen, Wen Yong-Wei.
+Copyright (c) 2021 Chiu Yen-Chen, Swen Sun-Yen, Wen Yong-Wei, Yuan Wei-Chen.
 All rights reserved.
 Use of this source code is governed by a BSD-style license that can be
 found in the LICENSE file. See the AUTHORS file for names of contributors.
@@ -15,71 +15,72 @@ found in the LICENSE file. See the AUTHORS file for names of contributors.
 #include "SURLCreator.h"
 
 namespace shortlink {
-	// Database save the look-up table to link each SURL to there original URL
-	//
-	// User can only do creating and reading the data.
-	// In create operation:
-	//		Data not exist: call insert operation.
-	//		Data exist: call update operation.
-	//
-	// public implement: Create, Read
-	// Create:		// if rawURL already exist, do UpdateSURL, else
-	// InsertSURL
-	//		string raw_url = "www.google.com"
-	//		string short_url = "";
-	//		string createStatus = database.CreateSURL(raw_url);
-	//		if (createStatus != "")
-	//			short_url = createStatus;
-	//	Read:
-	//		string raw_url = "";
-	//		string findStatus = database.ReadSURL(short_url);
-	//		if (findStatus != "")
-	//			raw_url = findStatus;
+// Database save the look-up table to link each SURL to there original URL
+//
+// User can only do creating and reading the data.
+// In create operation:
+//		Data not exist: call insert operation.
+//		Data exist: call update operation.
+//
+// public implement: Create, Read
+// Create:		// if rawURL already exist, do UpdateSURL, else
+// InsertSURL
+//		string raw_url = "www.google.com"
+//		string short_url = "";
+//		string createStatus = database.CreateSURL(raw_url);
+//		if (createStatus != "")
+//			short_url = createStatus;
+//	Read:
+//		string raw_url = "";
+//		string findStatus = database.ReadSURL(short_url);
+//		if (findStatus != "")
+//			raw_url = findStatus;
 
-	class Database {
-	public:
-		Database();
+class Database {
+public:
+  // default set last expiration to now
+  Database() { last_expiration = boost::gregorian::day_clock::local_day(); }
 
-		// delete copy constructor
-		Database(const Database&) = delete;
+  // delete copy constructor
+  Database(const Database &) = delete;
 
-		// delete copy operator
-		Database& operator=(const Database&) = delete;
+  // delete copy operator
+  Database &operator=(const Database &) = delete;
 
-		// lock the mutex when it is inserting.
-		// There is only one inserting in the same time.
-		bool InsertSURL(std::unique_ptr<SURL> surl);
+  // Input a rawURL, return SURL if create is successful. Return "" if create
+  // fail. Encode the rawURL to SURL and check whether it exists. If it exists
+  // update expire time, else create the URL.
+  std::string CreateSURL(const std::string &raw_url);
 
-		// Input a rawURL, return SURL if create is successful. Return "" if create
-		// fail. Encode the rawURL to SURL and check whether it exists. If it exists
-		// update expire time, else create the URL.
-		std::string CreateSURL(const std::string& raw_url);
+  // Read the lut whether URL exists. Return rawURL if SURL exists. Return "" if
+  // SURL doesn't exist.
+  std::string ReadSURL(const std::string &surl);
 
-		// Read the lut whether URL exists. Return rawURL if SURL exists. Return "" if
-		// SURL doesn't exist.
-		std::string ReadSURL(const std::string& surl) const;
+private:
+  // lock the mutex when it is inserting.
+  // There is only one inserting in the same time.
+  bool InsertSURL(const std::string &short_url, std::unique_ptr<SURL> surl);
 
-	private:
-		// If there is a rawURL which encoding to a SURL that exists in LUT,
-		// Update its expire time and don't need to create new one in LUT.
-		bool UpdateSURL(std::unique_ptr<SURL> surl, Date expire);
+  // If there is a rawURL which encoding to a SURL that exists in LUT,
+  // Update its expire time and don't need to create new one in LUT.
+  bool UpdateSURL(std::unique_ptr<SURL> surl, const Date &expire);
 
-		// It is for daily work. Check if the expire time of the SURL is over.
-		bool CheckSURLExpiration();
+  // It is for daily work. Check if the expire time of the SURL is over.
+  bool CheckSURLExpiration();
 
-		// If the expire time of the SURL is over, erase if in lut.
-		bool DeleteSURL();
+  // If the expire time of the SURL is over, erase if in lut.
+  bool DeleteSURL();
 
-		// Encode the rawURL to the SURL and set the expire time to now.
-		static std::unique_ptr<SURL> EncodeSURL(const std::string& raw_url);
+  // The look up table of the SURL. Use SURL as key to find the original url.
+  std::unordered_map<std::string, std::unique_ptr<SURL>> lut;
 
-		// The look up table of the SURL. Use SURL as key to find the original url.
-		std::unordered_map<std::string, std::unique_ptr<SURL>> lut;
+  // Record the last time we check the surl expiration
+  Date last_expiration;
 
-		// Lock to prevent multiple user from modifying the SURL.
-		// There is only one insertion in the same time.
-		std::mutex write_Lock;
-	};
+  // Lock to prevent multiple user from modifying the SURL.
+  // There is only one modification in the same time.
+  std::mutex write_lock;
+};
 } // namespace shortlink
 
 #endif
